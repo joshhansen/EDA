3ff59546b35c5e6c3719fc3a65b946f18c7671bb
Josh Hansen <hansen.joshuaa@gmail.com>
2012-01-31 15:43:03 -0700

    Speed things up a little

--- src/jhn/eda/processor/MongoTopicWordMatrixVisitor2.java
+++ src/jhn/eda/processor/MongoTopicWordMatrixVisitor2.java
@@ -8,11 +8,10 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import jhn.eda.Util;
 import cc.mallet.types.Alphabet;
@@ -91,16 +90,37 @@ public class MongoTopicWordMatrixVisitor2 extends Visitor {
 
 	@Override
 	public void visitWord(String word) {
-		if(stopwords.contains(word)) return;
+		if(word.isEmpty() || stopwords.contains(word)) return;
 		
 		Integer count = currentLabelWordCounts.get(word);
 		count = count==null? 1 : count+1;
 		currentLabelWordCounts.put(word, count);
 	}
 	
+	@Override
+	public void afterLabel() {
+		for(Entry<String,Integer> entry : currentLabelWordCounts.entrySet()) {
+			DBObject dbWord = cache.getWordFromDB(entry.getKey());
+			
+			final String labelIdx = String.valueOf(currentLabelIdx);
+			if(dbWord.containsField(labelIdx)) throw new IllegalArgumentException();
+			
+			dbWord.put(labelIdx, entry.getValue());
+			c.save(dbWord);
+		}
+		
+		currentLabelWordCounts.clear();
+	}
+
+	@Override
+	public void afterEverything() {
+		cache.close();
+		m.close();
+	}
+	
 	private class WordCache {
-		private static final int size = 1000;
-		private static final int numToRetain = 800;
+		private static final int size = 5000;
+		private static final int numToRetain = 1000;
 		private final Map<String,DBObject> elements = new HashMap<String,DBObject>();
 		
 		private Comparator<DBObject> comp = new Comparator<DBObject>(){
@@ -132,29 +152,29 @@ public class MongoTopicWordMatrixVisitor2 extends Visitor {
 			return wordObj;
 		}
 		
-		private void printCache(DBObject[] sortedWords) {
-			System.out.println("Head:");
-			for(int i = sortedWords.length - 1; i > sortedWords.length - 11; i--) {
-				DBObject wordObj = sortedWords[i];
-				String word = wordObj.get("_word").toString();
-				int count = wordObj.keySet().size();
-				System.out.println("\t"+word + ": " + count);
-			}
-			
-			System.out.println("Tail:");
-			for(int i = 0; i < 10; i++) {
-				DBObject wordObj = sortedWords[i];
-				String word = wordObj.get("_word").toString();
-				int count = wordObj.keySet().size();
-				System.out.println("\t"+word + ": " + count);
-			}
-		}
+//		private void printCache(DBObject[] sortedWords) {
+//			System.out.println("Head:");
+//			for(int i = sortedWords.length - 1; i > sortedWords.length - 11; i--) {
+//				DBObject wordObj = sortedWords[i];
+//				String word = wordObj.get("_word").toString();
+//				int count = wordObj.keySet().size();
+//				System.out.println("\t"+word + ": " + count);
+//			}
+//			
+//			System.out.println("Tail:");
+//			for(int i = 0; i < 10; i++) {
+//				DBObject wordObj = sortedWords[i];
+//				String word = wordObj.get("_word").toString();
+//				int count = wordObj.keySet().size();
+//				System.out.println("\t"+word + ": " + count);
+//			}
+//		}
 		
 		private void flushCache() {
 			DBObject[] sortedWords = elements.values().toArray(new DBObject[0]);
 			Arrays.sort(sortedWords, comp);
 			
-			printCache(sortedWords);
+//			printCache(sortedWords);
 			
 			Set<DBObject> wordsToRetain = new HashSet<DBObject>();
 			
@@ -162,12 +182,6 @@ public class MongoTopicWordMatrixVisitor2 extends Visitor {
 				wordsToRetain.add(sortedWords[i]);
 			}
 			
-//			int retained = 0;
-//			for(DBObject wordObj : sortedWords) {
-//				wordsToRetain.add(wordObj);
-//				retained++;
-//				if(retained >= numToRetain) break;
-//			}
 			
 			Iterator<Entry<String,DBObject>> it = elements.entrySet().iterator();
 			while(it.hasNext()) {
@@ -179,28 +193,32 @@ public class MongoTopicWordMatrixVisitor2 extends Visitor {
 				}
 			}
 		}
+		
+		public void close() {
+			Iterator<Entry<String,DBObject>> it = elements.entrySet().iterator();
+			while(it.hasNext()) {
+				Entry<String,DBObject> entry = it.next();
+				DBObject wordObj = entry.getValue();
+				c.save(wordObj);
+				it.remove();
+			}
+		}
 	}//end class WordCache
 	
-	
-	
-	
-	@Override
-	public void afterLabel() {
-		for(Entry<String,Integer> entry : currentLabelWordCounts.entrySet()) {
-			DBObject dbWord = cache.getWordFromDB(entry.getKey());
-			
-			final String labelIdx = String.valueOf(currentLabelIdx);
-			if(dbWord.containsField(labelIdx)) throw new IllegalArgumentException();
-			
-			dbWord.put(labelIdx, entry.getValue());
-			c.save(dbWord);
-		}
-		
-		currentLabelWordCounts.clear();
-	}
-
-	@Override
-	public void afterEverything() {
-		m.close();
-	}
+//	private Map<String,List<Integer>> groupedCounts() {
+//		Map<String,List<Integer>> groupedCounts = new HashMap<String,List<Integer>>();
+//		
+//		for(Entry<Integer,Integer> entry : labelWordCounts.entrySet()) {
+//			final Integer wordIdx = entry.getKey();
+//			final String count = entry.getValue().toString();
+//			List<Integer> group = groupedCounts.get(count);
+//			if(group == null){
+//				group = new ArrayList<Integer>();
+//				groupedCounts.put(count, group);
+//			}
+//			group.add(wordIdx);
+//		}
+//		
+//		return groupedCounts;
+//	}
 }
