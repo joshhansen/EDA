a5f829d386172bcbfe2c64a255d200ca14493993
Josh Hansen <hansen.joshuaa@gmail.com>
2012-02-21 13:03:45 -0700

    Add some useful scripts

---
+++ import.sh
@@ -0,0 +1,1 @@
+../topicalguide/tools/mallet/mallet import-dir --input ./toy_dataset --output ./toy_dataset.mallet --remove-stopwords --keep-sequence --set-source-by-name --token-regex "[a-zA-Z]+" --source-name-prefix "file:/home/jjfresh/Projects/eda_java/toy_dataset/"
