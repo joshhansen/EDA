ecf4d4bdbe6421d527779c1f3f933a98ae572031
Josh Hansen <hansen.joshuaa@gmail.com>
2012-02-28 18:20:28 -0700

    Add a compilation script

---
+++ compile_counter.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -cp /home/jjfresh/Libraries/bzip2:../External/mallet-2.0.7/class:../External/mallet-2.0.7/lib/mallet-deps.jar:/home/jjfresh/Libraries/nxparser-1.2.1.jar:/home/jjfresh/Libraries/commons-lang3-3.1-src/bin:/home/jjfresh/Libraries/mongo-java-driver/bin:/home/jjfresh/Libraries/bliki_3.0.16/bliki-core/target/bliki-core-3.0.16.jar:/home/jjfresh/Libraries/wikixml-j/bin -sourcepath ./src -d ./bin ./src/jhn/wp/CountArticles.java
