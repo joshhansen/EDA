c2d2a8da4ce1e539954f7f6cb046a2aa60040a59
Josh Hansen <hansen.joshuaa@gmail.com>
2012-03-09 15:11:02 -0700

    Refresh some scripts

--- scripts/compile_counter.sh
+++ scripts/compile_counter.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-javac -cp /home/jjfresh/Libraries/bzip2:../External/mallet-2.0.7/class:../External/mallet-2.0.7/lib/mallet-deps.jar:/home/jjfresh/Libraries/nxparser-1.2.1.jar:/home/jjfresh/Libraries/commons-lang3-3.1-src/bin:/home/jjfresh/Libraries/mongo-java-driver/bin:/home/jjfresh/Libraries/bliki_3.0.16/bliki-core/target/bliki-core-3.0.16.jar:/home/jjfresh/Libraries/wikixml-j/bin:$HOME/Libraries/lucene-3.5.0/lucene-core-3.5.0.jar -sourcepath ./src -d ./bin ./src/jhn/wp/CountArticles.java
+LIB=$HOME/Libraries
+EXT=$HOME/Projects/External
+javac -cp $LIB/bzip2:$EXT/mallet-2.0.7/class:$EXT/mallet-2.0.7/lib/mallet-deps.jar:$LIB/nxparser-1.2.1.jar:$LIB/commons-lang3-3.1-src/bin:$LIB/mongo-java-driver/bin:$LIB/bliki_3.0.16/bliki-core/target/bliki-core-3.0.16.jar:$LIB/wikixml-j/bin:$LIB/lucene-3.5.0/lucene-core-3.5.0.jar:$HOME/Projects/JhnCommon/bin -sourcepath ./src -d ./bin ./src/jhn/wp/CountArticles.java
--- scripts/count_articles.sh
+++ scripts/count_articles.sh
@@ -3,4 +3,4 @@
 MEM=14g
 LIB=$HOME/Libraries
 EXTPRJ=$HOME/Projects/External
-java -Xmx$MEM -cp bin:$LIB/bzip2:$EXTPROJ/mallet-2.0.7/class:$EXTPROJ/mallet-2.0.7/lib/mallet-deps.jar:$LIB/nxparser-1.2.1.jar:$LIB/commons-lang3-3.1-src/bin:$LIB/mongo-java-driver/bin:$LIB/bliki_3.0.16/bliki-core/target/bliki-core-3.0.16.jar:$LIB/wikixml-j/bin:$LIB/lucene-3.5.0/lucene-core-3.5.0.jar jhn.wp.CountArticles
+java -Xmx$MEM -cp bin:$LIB/bzip2:$EXTPROJ/mallet-2.0.7/class:$EXTPROJ/mallet-2.0.7/lib/mallet-deps.jar:$LIB/nxparser-1.2.1.jar:$LIB/commons-lang3-3.1-src/bin:$LIB/mongo-java-driver/bin:$LIB/bliki_3.0.16/bliki-core/target/bliki-core-3.0.16.jar:$LIB/wikixml-j/bin:$LIB/lucene-3.5.0/lucene-core-3.5.0.jar:$HOME/Projects/JhnCommon/bin jhn.wp.CountArticles
