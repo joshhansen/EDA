60ab5e04e3d05a20a0f10d6ef74c13f4dcfd2a3e
Josh Hansen <hansen.joshuaa@gmail.com>
2012-04-18 14:12:56 -0600

    Drop a bunch of unused code---MongoDB stuff, etc.

--- src/jhn/eda/mongo/MongoConf.java
+++
@@ -1,8 +0,0 @@
-package jhn.eda.mongo;
-
-public final class MongoConf {
-	public static final String server = "localhost";
-	public static final int port = 27017;
-	public static final String dbName = "dbpedia37";
-	public static final String collectionName = "type_topic_counts";
-}
--- src/jhn/wp/categories/CountCategories.java
+++
@@ -1,5 +0,0 @@
-package jhn.wp.categories;
-
-public class CountCategories {
-
-}
