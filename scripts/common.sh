561d61f31c0a4777b3491f991967c94dd0061944
Josh Hansen <hansen.joshuaa@gmail.com>
2012-05-05 15:40:54 -0600

    Refactor scripts

---
+++ scripts/common.sh
@@ -0,0 +1,20 @@
+# Versions
+LUCENE_V=3.6.0
+
+# Common Paths
+LIB=$HOME/Libraries
+PRJ=$HOME/Projects
+EXT=$PRJ/External
+
+JHNC=$PRJ/JhnCommon/bin
+BZIP2=$LIB/bzip2
+MALLET=$EXT/mallet-2.0.7/class
+MALLET_DEPS=$EXT/mallet-2.0.7/lib/mallet-deps.jar
+NXPARSER=$LIB/nxparser-1.2.1.jar
+LANG3=$LIB/commons-lang3-3.1-src/bin
+MONGO=$LIB/mongo-java-driver/bin
+LUCENE=$LIB/lucene-$LUCENE_V/lucene-core-$LUCENE_V.jar
+BLIKI=$LIB/bliki_3.0.16/bliki-core/target/bliki-core-3.0.16.jar
+WIKIXML_J=$LIB/wikixml-j/bin
+FASTUTIL=$LIB/fastutil-6.4.3-bin/fastutil-6.4.3.jar
+
--- scripts/query_lucene.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -Xmx14g -cp bin:/home/jjfresh/Libraries/bzip2:../External/mallet-2.0.7/class:../External/mallet-2.0.7/lib/mallet-deps.jar:/home/jjfresh/Libraries/nxparser-1.2.1.jar:/home/jjfresh/Libraries/commons-lang3-3.1-src/bin:/home/jjfresh/Libraries/mongo-java-driver/bin:/home/jjfresh/Libraries/bliki_3.0.16/bliki-core/target/bliki-core-3.0.16.jar:/home/jjfresh/Libraries/wikixml-j/bin:$HOME/Libraries/lucene-3.5.0/lucene-core-3.5.0.jar jhn.eda.LuceneTest
