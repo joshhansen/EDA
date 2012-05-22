64d82779f238b38659713a2b39e2d2b58c8d782a
Josh Hansen <hansen.joshuaa@gmail.com>
2012-05-22 17:13:15 -0600

    A centralized place for paths

---
+++ src/jhn/eda/Paths.java
@@ -0,0 +1,62 @@
+package jhn.eda;
+
+public final class Paths {
+	private Paths() {}
+	
+	public static String datasetsDir() {
+		return System.getenv("HOME") + "/Projects/eda/datasets";
+	}
+	
+		public static String datasetFilename(String datasetName) {
+			return datasetsDir() + "/" + datasetName + ".mallet";
+		}
+	
+	public static String outputDir() {
+		return System.getenv("HOME") + "/Projects/eda_output";
+	}
+	
+		public static String countsDir() {
+			return outputDir() + "/counts";
+		}
+		
+			public static String topicCountsDir() {
+				return countsDir() + "/topics";
+			}
+				public static String topicCountsFilename(String topicWordIdxName, String datasetName, int minCount) {
+					return topicCountsDir() + "/" + extractedDataID(topicWordIdxName, datasetName, minCount) + ".ser";
+				}
+			
+			public static String typeTopicCountsDir() {
+				return countsDir() + "/type_topics";
+			}
+				public static String typeTopicCountsFilename(String topicWordIdxName, String datasetName, int minCount) {
+					return typeTopicCountsDir() + "/" + extractedDataID(topicWordIdxName, datasetName, minCount) + ".ser";
+				}
+		
+		public static String indicesDir() {
+			return outputDir() + "/indices";
+		}
+		
+			public static String indexDir(String indexName) {
+				return indicesDir() + "/" + indexName;
+			}
+			
+				public static String topicWordIndicesDir() {
+					return indexDir("topic_words");
+				}
+			
+				public static String topicWordIndexDir(String name) {
+					return topicWordIndicesDir() + "/" + name;
+				}
+
+		public static String labelAlphabetsDir() {
+			return outputDir() + "/label_alphabets";
+		}
+			public static String labelAlphabetFilename(String topicWordIdxName, String datasetName, int minCount) {
+				return labelAlphabetsDir() + "/" + extractedDataID(topicWordIdxName, datasetName, minCount) + ".ser";
+			}
+	
+	public static String extractedDataID(String topicWordIdxName, String datasetName, int minCount) {
+		return topicWordIdxName + ":" + datasetName + "_min" + minCount;
+	}
+}
