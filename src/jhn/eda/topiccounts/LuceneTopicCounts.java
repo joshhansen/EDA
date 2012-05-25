0e4dc0c183e129dfd3c6cc7fe38c8d25288e6e1e
Josh Hansen <hansen.joshuaa@gmail.com>
2012-05-25 00:15:02 -0600

    A TopicCounts implementation backed by Lucene

---
+++ src/jhn/eda/topiccounts/LuceneTopicCounts.java
@@ -0,0 +1,36 @@
+package jhn.eda.topiccounts;
+
+import java.io.IOException;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.TermFreqVector;
+
+import jhn.wp.Fields;
+
+public class LuceneTopicCounts implements TopicCounts {
+	private final IndexReader topicWordIdx;
+	
+	public LuceneTopicCounts(IndexReader topicWordIdx) {
+		this.topicWordIdx = topicWordIdx;
+	}
+
+	@Override
+	public int topicCount(int topicID) throws TopicCountsException {
+		try {
+			final TermFreqVector termFreqVector = topicWordIdx.getTermFreqVector(topicID, Fields.text);
+			
+			int totalTermFreq = 0;
+			if(termFreqVector != null) {
+				int[] docTermFreqs = termFreqVector.getTermFrequencies();
+				for(int termFreq : docTermFreqs) {
+					totalTermFreq += termFreq;
+				}
+			}
+			
+			return totalTermFreq;
+		} catch (IOException e) {
+			throw new TopicCountsException(e);
+		}
+	}
+
+}
