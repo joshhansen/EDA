9f9e354afb13ed73896ad903b7fb0077a76926b8
Josh Hansen <hansen.joshuaa@gmail.com>
2012-11-27 22:02:29 -0700

    Throw in some useful comparators

--- src/jhn/eda/typetopiccounts/TopicCount.java
+++ src/jhn/eda/typetopiccounts/TopicCount.java
@@ -1,8 +1,25 @@
 package jhn.eda.typetopiccounts;
 
 import java.io.Serializable;
+import java.util.Comparator;
+
+import jhn.util.Util;
 
 public class TopicCount implements Serializable {
+	public static final Comparator<TopicCount> cmpTopic = new Comparator<TopicCount>(){
+		@Override
+		public int compare(TopicCount o1, TopicCount o2) {
+			return Util.compareInts(o1.topic, o2.topic);
+		}
+	};
+	
+	public static final Comparator<TopicCount> cmpCount = new Comparator<TopicCount>(){
+		@Override
+		public int compare(TopicCount o1, TopicCount o2) {
+			return Util.compareInts(o1.count, o2.count);
+		}
+	};
+	
 	private static final long serialVersionUID = 1L;
 	
 	public int topic;
