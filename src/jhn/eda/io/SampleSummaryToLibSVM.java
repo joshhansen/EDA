3e9c3d8a640b4588299da468f26dd3d3fa8bd1e8
Josh Hansen <hansen.joshuaa@gmail.com>
2012-11-27 22:08:01 -0700

    Convert sample summary files to libsvm format

---
+++ src/jhn/eda/io/SampleSummaryToLibSVM.java
@@ -0,0 +1,34 @@
+package jhn.eda.io;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import jhn.eda.tokentopics.DocTopicCounts;
+import jhn.eda.typetopiccounts.TopicCount;
+
+public class SampleSummaryToLibSVM {
+	public static void convert(String sampleSummaryFilename, String libSvmFilename) throws IOException, Exception {
+		List<TopicCount> topicCounts = new ArrayList<>();
+		try(SampleSummaryFileReader r = new SampleSummaryFileReader(sampleSummaryFilename);
+				LibSVMFileWriter w = new LibSVMFileWriter(libSvmFilename)) {
+			
+			for(DocTopicCounts dtc : r) {
+				topicCounts.clear();
+				while(dtc.hasNext()) {
+					int topic = dtc.nextInt();
+					int count = dtc.nextDocTopicCount();
+					topicCounts.add(new TopicCount(topic, count));
+				}
+				Collections.sort(topicCounts, TopicCount.cmpTopic);
+				
+				w.startDocument(dtc.docClass());
+				for(TopicCount tc : topicCounts) {
+					w.featureValue(tc.topic, tc.count);
+				}
+				w.endDocument();
+			}
+		}
+	}
+}
